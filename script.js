e25a57f585aefe86206431d3d3f70ac7e0479d37
Flavio Henrique da Silva <firmezinha1@hotmail.com>
2025-08-10 01:16:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ function comprarIngresso() {
 
   if (usuarioLogado) {
     // Redireciona para a página do ingresso
-    window.location.href = "http://127.0.0.1:5501/show_da_banda/show_da_banda.html";
+    window.location.href = "/show_da_banda/show_da_banda.html";
   } else {
     // Redireciona para a página de login
     window.location.href = "/login/login.html";
   }
 }
 
+function criarEventos() {
+  window.location.href = "/criar_evento/criar_evento.html";
+}
+
+function meusEventos() {
+  window.location.href = "/meus_eventos/meus_eventos.html";
+}
+
+function meusIngressos() {
+  window.location.href = "/meus_ingressos/meus_ingressos.html";
+}
+
+function cadastro() {
+  window.location.href = "/cadastro/cadastro.html";
+}
